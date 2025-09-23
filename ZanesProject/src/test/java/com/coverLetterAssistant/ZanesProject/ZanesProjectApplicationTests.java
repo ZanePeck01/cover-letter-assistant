df5ce65779b98f792813ef080b2e029baee8db7a
Zane Peckenpaugh <zanepeck01@gmail.com>
2025-09-23 18:21:21 -0400

    Added spring boot base

---
+++ ZanesProject/src/test/java/com/coverLetterAssistant/ZanesProject/ZanesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coverLetterAssistant.ZanesProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZanesProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
